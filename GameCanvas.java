386e6d204b840e45d78bedc6351578b8b20cb46a
nheoxoz <nheo@janks.org>
2022-05-17 11:59:27 +0800

    hello

--- GameCanvas.java
+++ GameCanvas.java
@@ -1,5 +1,7 @@
 //This class extendsJComponent and overridesthepaintComponent method in
 //order to create the custom drawing.
 public class GameCanvas{
-    
+    public static void main(String[] args){
+
+    }
 }
