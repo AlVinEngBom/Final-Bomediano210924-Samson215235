368c481c43c9e4e83b3a1f8e0e9ddf39f7dee28a
AlVanilla <al.bomediano@obf.ateneo.edu>
2022-05-17 12:55:49 +0800

    Base Code

---
+++ GameCanvas.java
@@ -0,0 +1,14 @@
+//This class extendsJComponent and overrides thepaintComponent method in
+//order to create the custom drawing.
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GameCanvas extends JComponent{
+
+    public GameCanvas() {
+
+        setPreferredSize( new Dimension(1024, 768) );
+    }
+}
