d7657fc456f4496e078a2ba0c333e89a0c7f76cb
AlVanilla <al.bomediano@obf.ateneo.edu>
2022-05-20 18:33:33 +0800

    al-6:33pm-20/5/2022

--- Wall.java
+++ Wall.java
@@ -2,34 +2,18 @@ import java.awt.*;
 
 public class Wall{
     private int x,y,width,height;
-    private double xspeed,yspeed;
    
     //a constructor that accepts arguments for the x and y positions, width, height, 
     //and speed of the rectangle
-    public Wall(int x, int y, int width, int height, double xspeed, double yspeed){
+    public Wall(int x, int y, int width, int height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.xspeed = xspeed;
-        this.yspeed = yspeed;
     }
     //draws the rectangle
     public void draw(Graphics g){
         g.fillRect(x,y,width,height);
-    }
-    //acccessor methods to return all instance fields
-    public int getX(){
-        return x;
-    }
-    public int getY(){
-        return y;
-    }
-    public int getWidth(){
-        return width;
-    }
-    public int getHeight(){
-        return height;
     }
     //boolean for collision
     public boolean isColliding(Enemy other){
